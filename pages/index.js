e143cbf3e6e3934f7eb4bd867320f3fd6d644603
ガルシア・アグアよ、エティエン <garcia@in-g.jp>
2021-01-18 14:35:18 +0900

    set up polaris, moved some pages around

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+const { render } = require("react-dom");
+import {EmptyState, Layout, Page} from '@shopify/polaris';
+
+function Index(){
+  return(
+    <Page>
+      <Layout>
+        <EmptyState
+          heading="Manage your inventory transfers"
+          action={{content: 'select Products'}}
+          image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+        >
+          <p>Select Products</p>
+        </EmptyState>
+      </Layout>
+    </Page>
+  )
+}
+
+export default Index;
